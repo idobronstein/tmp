afe2d73b62bf8e1be6382e79591228c347a9528c
idobronstein <idobron123@gmail.com>
2018-08-04 18:24:18 +0300

    a

--- script_cifar100.sh
+++ script_cifar100.sh
@@ -1,8 +1,4 @@
-
-for entreg in 1 #1 0.1 10 0.01
-do
-  python train_cifar100.py --entropy_reg ${entreg} >& train10K_ent_manyKiter${entreg}.log 
-done
+python train_cifar100.py --entropy_reg 1 >& log.txt 
 
    
     
